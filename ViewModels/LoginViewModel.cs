6fc493eaa42d30a8f1093698d79edd527b94aaf0
duyd486 <90164590+duyd486@users.noreply.github.com>
2025-11-15 01:17:23 +0700

    setup login view + viewmodel

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Vibra_DesktopApp.ViewModels
+{
+    partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty] private string? emailText;
+        [ObservableProperty] private string? passwordText;
+
+
+
+        [RelayCommand]
+        private void Login()
+        {
+            if(EmailText == null || PasswordText == null)
+            {
+                MessageBox.Show("Please enter both email and password.");
+                return;
+            }
+
+            Console.WriteLine("Login button clicked");
+            MessageBox.Show("Login button clicked " + EmailText + " " + PasswordText);
+        }
+
+
+        public void SetPassword(string password)
+        {
+            PasswordText = password;
+        }
+    }
+}
